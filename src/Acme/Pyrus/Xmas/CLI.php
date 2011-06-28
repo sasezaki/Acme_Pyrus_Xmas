eadba3cd5e175168d761458f24e31db55b4242e8
sasezaki <sasezaki@gmail.com>
2011-06-28 22:10:28 +0900

    initial commit

---
+++ src/Acme/Pyrus/Xmas/CLI.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Acme\Pyrus\Xmas\CLI
+ *
+ * PHP version 5.3
+ *
+ * @category  Acme
+ * @package   Acme_Pyrus_Xmas
+ * @author    sasezaki <sasezaki@gmail.com>
+ * @copyright 2011 sasezaki
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      http://svn.php.net/repository/pear2/PEAR2_Acme_Pyrus_Xmas
+ */
+
+/**
+ * CLI class for Acme_Pyrus_Xmas
+ *
+ * @category  Acme
+ * @package   Acme_Pyrus_Xmas
+ * @author    sasezaki <handle@php.net>
+ * @copyright 2011 sasezaki
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      http://svn.php.net/repository/pear2/PEAR2_Acme_Pyrus_Xmas
+ */
+namespace Acme\Pyrus\Xmas;
+class CLI
+{
+    /**
+     *
+     */
+    public function xmas($frontend, $args, $options)
+    {
+$aa = <<<'AAA'
+Merry Christmas!
+　　 ∧ ∧
+　　(*ﾟーﾟ)
+☆☆"ＵＵ"ﾐ☆☆
+‥‥‥▲‥*‥‥
+‥*‥▲▲‥‥‥
+‥‥▲▲▲‥‥*
+*‥▲▲▲▲‥‥
+‥‥‥■‥‥*‥
+AAA;
+        echo $aa;
+
+    }
+}
